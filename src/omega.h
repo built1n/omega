f0963f8cd0e0e21dfaca004e4f00b8eb6afbf33e
Franklin Wei <frankhwei536@gmail.com>
2014-03-28 19:15:53 -0400

    Rewrote z80_flags, added include guards, fixed typedef

--- src/omega.h
+++ src/omega.h
@@ -1,17 +1,22 @@
+#ifndef OMEGA_H_
+#define OMEGA_H_
 #include <stdint.h>
-typedef byte uint8_t;
-typedef word uint16_t;
+#include <stddef.h>
+typedef uint8_t byte;
+typedef uint16_t word;
 typedef struct z80_flags {
-  byte z:1;
-  byte s:1;
-  byte p:1;
-  byte c:1;
-  byte ac:1;
-  byte junk:3;
-} __attribute__(packed) z80_flags;
+  byte zero:1;
+  byte sign:1;
+  byte f5:1;
+  byte hc:1;
+  byte f3:1;
+  byte pv:1; // parity or overflow, set for even parity
+  byte n:1;
+  byte carry:1;
+} __attribute__((packed)) z80_flags;
 typedef struct z80_ctx {
   byte a, b, c, d, e, h, l, i, r;
-  z80_flags f;
+  z80_flags flags, altflags;
   word sp, pc, ix, iy;
   byte* memory;
   // no need for memsize, assume 0xFFFF
@@ -22,3 +27,4 @@ void omega_exec(z80_ctx*);
 #define Z80_SUBTRACT 0x40
 #define Z80_HALF_CARRY 0x20
 #define Z80_CARRY 0x10
+#endif
